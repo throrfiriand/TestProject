26f7206252c1cb8ec3725d32616a64b29d9b0a25
throrfiriand <cemdilektasli@gmail.com>
2024-10-31 16:46:59 +0300

    Game final

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2d;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0) //it is positive
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < 0) //it is negative
+        {
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
+        }
+
+        else if (Input.GetAxis("Vertical") > 0) //it is positive
+        {
+            rigidbody2d.velocity = new Vector2(0f, speed);
+        }
+        else if (Input.GetAxis("Vertical") < 0) //it is negative
+        {
+
+            rigidbody2d.velocity = new Vector2(0f, -speed);
+        }
+        else if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        {
+            //stop
+            rigidbody2d.velocity = new Vector2(0f, 0f);
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Door")
+        Debug.Log("Level Complete!!!");
+    }
+
+}
+
+
+        
+
+
