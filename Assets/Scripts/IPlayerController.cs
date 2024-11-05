6dcd7136f872109f23e48ea362725212c97792e8
throrfiriand <cemdilektasli@gmail.com>
2024-11-05 13:38:38 +0300

    Added GameWon/GameLost Panels

---
+++ Assets/Scripts/IPlayerController.cs
@@ -0,0 +1,4 @@
+﻿public interface IPlayerController
+{
+    void RestartGame();
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IPlayerController
 {
+    public GameObject GameWonPanel;
+    public GameObject GameLostPanel;
     public Rigidbody2D rigidbody2d;
     public float speed;
-
-    void Start()
-    {
-
-    }
+    private bool isGameOver = false;
     void Update()
     {
+        if (isGameOver == true)
+
+        {
+            return;
+
+        }
+
         if (Input.GetAxis("Horizontal") > 0) //it is positive
         {
             rigidbody2d.velocity = new Vector2(speed, 0f);
@@ -37,12 +43,33 @@ public class PlayerController : MonoBehaviour
             //stop
             rigidbody2d.velocity = new Vector2(0f, 0f);
         }
-
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Door")
-        Debug.Log("Level Complete!!!");
-    }
+        {
+            Debug.Log("Level Complete!!");
+            GameWonPanel.SetActive(true);
+            isGameOver = true;
+        }
 
+        else if (other.tag == "Enemy")
+        {
+            Debug.Log("Level Lost!!");
+            GameLostPanel.SetActive(true);
+            isGameOver = true;
+        }
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Button Clicked");
+        Time.timeScale = 1;
+    }
 }
+
+
+
+
+
